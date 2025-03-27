e74a1842a1e0c4d7b63f73027bba14a8686c4b1d
gnubrio <whitecc2005@proton.me>
2025-03-27 12:54:53 -0700

    added file extensions

--- app/config.py
+++ app/config.py
@@ -7,5 +7,40 @@ class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
-    ALLOWED_EXTENSIONS = {".png", ".jpeg", ".jpg", ".gif"}
-    WORKER_STARTED = False
+    ALLOWED_EXTENSIONS = {
+        ".png",
+        ".jpeg",
+        ".jpg",
+        ".gif",
+        ".svg",
+        ".webp",
+        ".css",
+        ".js",
+        ".html",
+        ".htm",
+        ".woff",
+        ".woff2",
+        ".ttf",
+        ".otf",
+        ".mp3",
+        ".ogg",
+        ".wav",
+        ".aac",
+        ".mp4",
+        ".webm",
+        ".ogg",
+        ".pdf",
+        ".txt",
+        ".docx",
+        ".xlsx",
+        ".zip",
+        ".tar.gz",
+        ".tar.bz2",
+        ".tar.xz",
+        ".php",
+        ".py" ".rust",
+        ".c",
+        ".h",
+        ".cpp",
+        ".hpp",
+    }
